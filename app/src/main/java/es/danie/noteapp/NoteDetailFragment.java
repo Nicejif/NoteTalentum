30927b0aaadab9c84c22da5ae0aa9f939b4a0a4b
Nicejif <danielioniceanu@gmail.com>
2017-01-12 12:09:22 +0100

    Bottons

--- app/src/main/java/es/danie/noteapp/NoteDetailFragment.java
+++ app/src/main/java/es/danie/noteapp/NoteDetailFragment.java
@@ -1,11 +1,14 @@
 package es.danie.noteapp;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -13,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class NoteDetailFragment extends Fragment {
 
+    EditText titleText;
+    EditText descriptionText;
 
     public NoteDetailFragment() {
         // Required empty public constructor
@@ -22,8 +27,36 @@ public class NoteDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_detail, container, false);
+                View view= inflater.inflate(R.layout.fragment_note_detail, container, false);
+                return view;
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        //save all from screen to disk: fragment is going to be destroy
+
+        saveAllDataToDisk();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // load data to show on screen (if any)
+
+        loadAllDataToDisk();
+    }
+
+    private void saveAllDataToDisk() {
+    }
+
+    private void loadAllDataToDisk() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String noteTitle=preferences.getString("NOTE_TITLE", "");
+        String noteDescription= preferences.getString("NOTE_DESCRIPTION", "");
     }
 
 }
