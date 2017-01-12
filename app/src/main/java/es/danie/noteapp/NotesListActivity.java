71d9833df74d0ebe09b42391a1bde152f671a6f1
Nicejif <danielioniceanu@gmail.com>
2017-01-12 11:38:23 +0100

    Add_button_activity

--- app/src/main/java/es/danie/noteapp/NotesListActivity.java
+++ app/src/main/java/es/danie/noteapp/NotesListActivity.java
@@ -1,5 +1,6 @@
 package es.danie.noteapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -29,7 +30,10 @@ public class NotesListActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.menu_main_action_add_note) {
 
-            //TODO: me queda por abrir la pantalla de las notas.
+
+            Intent i = new Intent(NotesListActivity.this,NoteDetailActivity.class);
+            startActivity(i);
+
             //FRAGMENTO: Cacho de interfaz
 
         }
